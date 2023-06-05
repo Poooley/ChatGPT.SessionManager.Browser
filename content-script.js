6a4d1e3f7094772005e8cad4d829a0d60e167426
Paul Haidinger <paul@haidinger.me>
2023-06-05 22:24:18 +0200

    ✨ better disabling of button

--- content-script.js
+++ content-script.js
@@ -1,8 +1,6 @@
 (async () => {
   const { SessionManagerWebSocket } = await import(chrome.runtime.getURL('SessionManagerWebSocket.js'));
-  const { SessionManagerApi } = await import(chrome.runtime.getURL('SessionManagerApi.js'));
 
-  sessionManagerApi = new SessionManagerApi();
   let apiKey;
   let sessionId;
 
@@ -12,7 +10,7 @@
     });
 
     chrome.storage.local.get('sessionId', function (result) {
-      sessionId = result.sessionId; 6
+      sessionId = result.sessionId;
     });
 
     if (apiKey && sessionId) {
@@ -23,15 +21,55 @@
     await new Promise(resolve => setTimeout(resolve, 2500));
   }
 
+  let promptTextarea = null;
+  let promptDiv = null;
+
+  while (!promptTextarea || !promptDiv) {
+    promptTextarea = document.getElementById('prompt-textarea');
+    if (promptTextarea) {
+      promptDiv = promptTextarea.parentNode;
+    }
+
+    if (!promptTextarea || !promptDiv) {
+      console.log("Waiting for the page to load...");
+      await new Promise(resolve => setTimeout(resolve, 500));
+    }
+  }
+
+  console.debug('Found prompt textarea:', promptTextarea);
+  const buttons = promptDiv.querySelectorAll('button');
+
+  promptTextarea.addEventListener('input', () => {
+    buttons.forEach(button => {
+      setTimeout(() => {
+
+        if (button.disabled !== this.isLockedState) {
+          button.disabled = this.isLockedState;
+          console.debug('Delayed check: Setting button disabled to', this.isLockedState);
+        }
+      }, 1000);
+
+      console.debug('Typing detected: Setting button disabled to', this.isLockedState);
+      button.disabled = this.isLockedState;
+    });
+  });
+
   let sessionManagerWebSocket = new SessionManagerWebSocket();
   sessionManagerWebSocket.connectWebSocket(sessionId);
   sessionManagerWebSocket.onLockStatusChanged = checkLockStatus;
+
 })();
 
+window.isLockedState = false;
+
 function checkLockStatus(isLocked) {
-  console.info(`Lock status changed to ${isLocked}`);
-  const buttons = document.querySelectorAll('button.absolute, button.btn');
+  window.isLockedState = isLocked;
+
+  console.info(`Lock status changed to ${window.isLockedState}`);
+  const promptDiv = document.getElementById('prompt-textarea').parentNode;
+  const buttons = promptDiv.querySelectorAll('button');
+
   buttons.forEach(button => {
-    button.disabled = isLocked;
+    button.disabled = isLockedState;
   });
 }
